adb3e1d936610f4070637f5bd29f56499a314d1d
radebeneo <radebeneo17@gmail.com>
2026-02-20 14:43:49 +0200

    refactored ballons to snow

--- laptop_prediction.py
+++ laptop_prediction.py
@@ -30,7 +30,7 @@ if prediction:
 
     x1 = pd.DataFrame([[processor_speed, ram_size, storage_capacity]], columns=["Processor_Speed", "RAM_Size", "Storage_Capacity"])
     prediction_val = model.predict(x1)[0]
-    st.balloons()
+    st.snow()
     st.write(f"Price estimate for your laptop is {prediction_val:,.2f}")
 
 else:
